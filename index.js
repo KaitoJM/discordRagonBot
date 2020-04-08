47fcc5107d4f7699bd3724667d4c5b6db65a2f9b
JM Home <johnmarkmancol@gmail.com>
2020-04-08 14:08:17 +0800

    refactored

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const Bot = new Discord.Client();
 
-const token = 'Njk3MDgyMTk4MzYwMTk1MjAz.XoyIIQ.FK8-61KfNaAQ_4AcT2Idfl-VwG8';
+const token = 'Njk3MDgyMTk4MzYwMTk1MjAz.Xo1nLA.Z3RMpDVRnyMcBMpdsNOPYYBVSiI';
+const prefix = '!';
 
 Bot.on('ready', () => {
     console.log('Your Bot is online! Gago!');
@@ -11,16 +12,22 @@ Bot.on('message', msg => {
     console.log(msg.author);
     let message_id = msg.id;
 
-    if ((msg.content).includes('!bulig')) {
-        msg.reply('!sd --> Self destruct your message in 5 seconds. \nMoi bayot!');
-    }
+    let args = msg.content.substring(prefix.length).split(" ");
 
-    if ((msg.content).includes('!sd') && (msg.author.username != 'JMBot')) {
-        msg.reply('Your message will self-destruct in 5 seconds...');
+    switch (args[0]) {
+        case 'bulig':
+            msg.reply('!sd --> Self destruct your message in 5 seconds. \nMoi bayot!');
+            break;
 
-        setTimeout(() => {
-            msg.delete();
-        }, 5000);
+        case 'sd':
+            if (msg.author.username != 'JMBot') {
+                msg.reply('Your message will self-destruct in 5 seconds...');
+
+                setTimeout(() => {
+                    msg.delete();
+                }, 5000);
+            }
+            break;
     }
 
     if(msg.author.username == 'JMBot') {
